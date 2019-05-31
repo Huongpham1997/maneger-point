4e468c10ed25af9dcd43ae5b71309925733e000a
Anthony <tuantrylook@gmail.com>
2019-05-31 14:25:18 +0700

    add

--- app/extend/header.php
+++ app/extend/header.php
@@ -4,19 +4,19 @@
     <title>Quản lý điểm học sinh</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../app/statics/css/bootstrap.min.css">
-    <link href="../app/statics/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
-    <link rel="stylesheet" href="../app/statics/css/mycss.css">
-    <link rel="stylesheet" href="../app/statics/css/mycss2.css">
-    <link rel="stylesheet" href="../app/statics/css/table-css.css">
+    <link rel="stylesheet" href="../statics/css/bootstrap.min.css">
+    <link href="../statics/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="../statics/css/mycss.css">
+    <link rel="stylesheet" href="../statics/css/mycss2.css">
+    <link rel="stylesheet" href="../statics/css/table-css.css">
     <!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
-    <script src="../app/statics/js/jquery.min.js"></script>
-    <script src="../app/statics/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="../app/statics/js/bootstrap-datetimepicker.js" charset="UTF-8"></script>
-	<script type="text/javascript" src="../app/statics/js/locales/bootstrap-datetimepicker.fr.js" charset="UTF-8"></script>
-    <script src="../app/statics/js/myjs.js"></script>
+    <script src="../statics/js/jquery.min.js"></script>
+    <script src="../statics/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../statics/js/bootstrap-datetimepicker.js" charset="UTF-8"></script>
+	<script type="text/javascript" src="../statics/js/locales/bootstrap-datetimepicker.fr.js" charset="UTF-8"></script>
+    <script src="../statics/js/myjs.js"></script>
     
 </head>
-<div><img src="../app/statics/images/banner1.jpg" id="banner" class="responsive"></div>
+<div><img src="../statics/images/banner1.jpg" id="banner" class="responsive"></div>
 <body>
     
