7f0ff6681a9d58268e4cd33e09f4936f99085d1a
Pham Van Tuan <tuantrylook@gmail.com>
2019-06-01 09:14:35 +0700

    unitest

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -5,7 +5,7 @@
   <div class="row">
     <div class="col-xs-6 col-md-3">
       <div class="thumbnail">
-        <img src="../app/statics/images/index1.jpg">
+        <img src="/statics/images/index1.jpg">
         <div class="caption">
           <h3>Bức thư kỳ lạ của thầy hiệu trưởng Singapore gửi phụ huynh</h3>
           <p>Các em còn nhỏ, điều các em cần là hạnh phúc, sự quan tâm của gia đình. Không một đứa trẻ nào cũng có thể trở thành “con nhà người ta”. Một đứa trẻ chỉ biết ...</p>
@@ -16,7 +16,7 @@
       </div>
       <div class="col-xs-6 col-md-3">
         <div class="thumbnail">
-          <img src="../app/statics/images/index2.jpg">
+          <img src="/statics/images/index2.jpg">
           <div class="caption">
             <h3>Danh sách giải học sinh giỏi toàn trường ngày 20/05/2016 </h3>
             <p>Sau những ngày thi cử mệt mỏi và vì đợi chờ danh sách giải. Hôm nay đã có danh sách giải học sinh giỏi toàn trường ngày 20/05/2016. Xin chúc mừng các em có tên trong danh sách. Các em đã cố gắng rất nhiều !</p>
@@ -26,7 +26,7 @@
       </div>
       <div class="col-xs-6 col-md-3">
         <div class="thumbnail">
-          <img src="../app/statics/images/index3.jpg">
+          <img src="/statics/images/index3.jpg">
           <div class="caption">
             <h3>Trường THCS Dũng Tiến kỉ niệm ngày thành lập đoàn 26/03</h3>
             <p>Tháng ba – tuổi trẻ tự hào, náo nức thực hiện “Hành trình tuổi trẻ làm theo lời Bác”. Hòa chung nhịp đập của tuổi trẻ cả nước, hôm nay Trường THCS Dũng Tiến tổ chức hội diễn thể dục kỷ niệm ngày  ....</p>
@@ -36,7 +36,7 @@
       </div>
       <div class="col-xs-6 col-md-3">
         <div class="thumbnail">
-          <img src="../app/statics/images/index4.jpg">
+          <img src="/statics/images/index4.jpg">
           <div class="caption">
             <h3>Hội thi Văn nghệ chào mừng ngày nhà giáo Việt Nam</h3>
             <p>Sáng ngày 18/11/2015 tại sân trường THCS Dũng Tiến đã tổ chức hội thi Văn nghệ chào mừng kỉ niệm 33 năm ngày nhà giáo Việt Nam 20/11/1982 – 20/11/2015.
@@ -52,13 +52,13 @@
     <div class="col-xs-6 col-md-6">
       <h3>Hội thi chào mừng ngày 20-10</h3>
       <video width="550" controls>
-        <source src="../app/statics/images/vn.mp4" type="video/mp4">
+        <source src="/statics/images/vn.mp4" type="video/mp4">
       </video>
     </div>
     <div class="col-xs-6 col-md-6">
       <h3>Hội thi cô giáo tài năng duyên dáng</h3>
       <video width="550" controls>
-        <source src="../app/statics/images/mua.mp4" type="video/mp4">
+        <source src="/statics/images/mua.mp4" type="video/mp4">
       </video>
     </div>
     
