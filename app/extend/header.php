e5c52df0aac1b7ba9f8eb302fa4a0b9c32a9c0fd
Pham Tuan <tuantrylook@gmail.com>
2019-06-01 09:07:08 +0700

    refactor diretory

--- app/extend/header.php
+++ app/extend/header.php
@@ -5,18 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
     <link rel="stylesheet" href="../statics/css/bootstrap.min.css">
-    <link href="../statics/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
-    <link rel="stylesheet" href="../statics/css/mycss.css">
-    <link rel="stylesheet" href="../statics/css/mycss2.css">
-    <link rel="stylesheet" href="../statics/css/table-css.css">
+    <link href="/statics/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="/statics/css/mycss.css">
+    <link rel="stylesheet" href="/statics/css/mycss2.css">
+    <link rel="stylesheet" href="/statics/css/table-css.css">
     <!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
-    <script src="../statics/js/jquery.min.js"></script>
-    <script src="../statics/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="../statics/js/bootstrap-datetimepicker.js" charset="UTF-8"></script>
-	<script type="text/javascript" src="../statics/js/locales/bootstrap-datetimepicker.fr.js" charset="UTF-8"></script>
-    <script src="../statics/js/myjs.js"></script>
+    <script src="/statics/js/jquery.min.js"></script>
+    <script src="/statics/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/statics/js/bootstrap-datetimepicker.js" charset="UTF-8"></script>
+	<script type="text/javascript" src="/statics/js/locales/bootstrap-datetimepicker.fr.js" charset="UTF-8"></script>
+    <script src="/statics/js/myjs.js"></script>
     
 </head>
-<div><img src="../statics/images/banner1.jpg" id="banner" class="responsive"></div>
+<div><img src="/statics/images/banner1.jpg" id="banner" class="responsive"></div>
 <body>
     
