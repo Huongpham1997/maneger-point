6d3f2e9eb8aa7e6f5103622b4d715136ec8f6514
Anthony <tuantrylook@gmail.com>
2019-06-03 13:48:43 +0700

    add

--- app/extend/footer.php
+++ app/extend/footer.php
@@ -1,107 +1,92 @@
 <?php
 
 ?>
-<div>
-	<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+<div class="copyright">
+    <div class="container">
 
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">   
-   
-<!--
-    instagram: www.instagram.com/programmingtutorial
-    site: programlamadersleri.net
--->
-
-
-    <div class="copyright">
-     <div class="container">
-       
-         <div class="row text-center">
-         	<p>Copyright © 2017 All rights reserved</p>
-         </div>
-         
- 	   </div>
-    </div>
-    
-    <a href="#" id="top" style="display: inline;">
-        <i class="fa fa-arrow-up" aria-hidden="true"></i>
-    </a>
-
-    <!-- Footer 2 -->
-    <footer style="margin-top:10px;">
-        
-        <div class="search-text"> 
-            <div class="container">
-                <div class="row text-center">
-                 
-                   <div class="form">
-                       <h4>SIGN UP TO OUR NEWSLETTER</h4>
-                        <form id="search-form" class="form-search form-horizontal">
-                            <input type="text" class="input-search" placeholder="Email Address">
-                            <button type="submit" class="btn-search">SUBMIT</button>
-                        </form>
-                    </div>
-                
-                </div>         
-            </div>     
+        <div class="row text-center">
+            <p>Copyright © 2017 All rights reserved</p>
         </div>
-        
-        <div class="footer-top2"> 
-           <div class="container">
-                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center">
-                   <a href="#"><i class="fa fa-facebook fa-2x"></i>Facebook</a>
+
+    </div>
+</div>
+
+<a href="#" id="top" style="display: inline;">
+    <i class="fa fa-arrow-up" aria-hidden="true"></i>
+</a>
+
+<!-- Footer 2 -->
+<footer style="margin-top:10px;">
+
+    <div class="search-text">
+        <div class="container">
+            <div class="row text-center">
+
+                <div class="form">
+                    <h4>SIGN UP TO OUR NEWSLETTER</h4>
+                    <form id="search-form" class="form-search form-horizontal">
+                        <input type="text" class="input-search" placeholder="Email Address">
+                        <button type="submit" class="btn-search">SUBMIT</button>
+                    </form>
                 </div>
-                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center">
-                   <a href="#"><i class="fa fa-twitter fa-2x"></i>Twitter</a>
-                </div>
-                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center">
-                   <a href="#"><i class="fa fa-flickr fa-2x"></i>Flickr</a>
-                </div>
-                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center">
-                    <a href="#"><i class="fa fa-tumblr fa-2x"></i>Tumblr</a>
-                </div>
-                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center">
-                    <a href="#"><i class="fa fa-github fa-2x"></i>Github</a>
-                </div>
-                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center">
-                    <a href="#"><i class="fa fa-google-plus fa-2x"></i>Google</a>
-                </div>
-           </div> 
-        </div>  
-        
-        <div class="container" style="border-top:1px solid grey;">
-            <div class="row text-center">   
-                <div class="col-lg-6 col-lg-offset-3">
-                      <ul class="menu">
-                                 
-                             <li>
-                                <a href="#">Home</a>
-                              </li>
-                                   
-                              <li>
-                                 <a href="#">About</a>
-                              </li>
-                                   
-                              <li>
-                                <a href="#">Blog</a>
-                              </li>
-                                   
-                              <li>
-                                 <a href="#">Gallery</a>
-                              </li>
-                                   
-                              <li>
-                                <a href="#">Contact</a>
-                             </li>
-                   
-                        </ul>
-                </div>
+
             </div>
         </div>
-        
-    </footer>
+    </div>
+
+    <div class="footer-top2">
+        <div class="container">
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center">
+                <a href="#"><i class="fa fa-facebook fa-2x"></i>Facebook</a>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center">
+                <a href="#"><i class="fa fa-twitter fa-2x"></i>Twitter</a>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center">
+                <a href="#"><i class="fa fa-flickr fa-2x"></i>Flickr</a>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center">
+                <a href="#"><i class="fa fa-tumblr fa-2x"></i>Tumblr</a>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center">
+                <a href="#"><i class="fa fa-github fa-2x"></i>Github</a>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center">
+                <a href="#"><i class="fa fa-google-plus fa-2x"></i>Google</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container" style="border-top:1px solid grey;">
+        <div class="row text-center">
+            <div class="col-lg-6 col-lg-offset-3">
+                <ul class="menu">
+
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Blog</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Gallery</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</footer>
 </div>
 
--- app/extend/header.php
+++ app/extend/header.php
@@ -4,21 +4,23 @@
     <title>Quản lý điểm học sinh</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../statics/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/statics/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/statics/css/new.css">
+    <link rel="stylesheet" href="/statics/css/style-responsive.css">
+    <link rel="stylesheet" href="/statics/css/font-awesome.css">
+    <link rel="stylesheet" href="/statics/css/jquery.bxslider.css">
     <link href="/statics/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
-    <link rel="stylesheet" href="/statics/css/mycss.css">
-    <link rel="stylesheet" href="/statics/css/mycss2.css">
-    <link rel="stylesheet" href="/statics/css/table-css.css">
-    <!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
+    
     <script src="/statics/js/jquery.min.js"></script>
     <script src="/statics/js/bootstrap.min.js"></script>
+    <script src="/statics/js/jquery.bxslider.min.js"></script>
+    <script src="/statics/js/jquery.mobile.custom.js"></script>
     <script type="text/javascript" src="/statics/js/bootstrap-datetimepicker.js" charset="UTF-8"></script>
 	<script type="text/javascript" src="/statics/js/locales/bootstrap-datetimepicker.fr.js" charset="UTF-8"></script>
     <script src="/statics/js/myjs.js"></script>
-    
+
 </head>
 <div>
     <img src="/statics/images/banner1.jpg" id="banner" class="responsive hidden-xs hidden-sm">
 </div>
 <body>
-    
