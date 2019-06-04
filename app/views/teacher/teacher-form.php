1573a0ac08b13dc0418ab531be8b9220c590885d
Hường <phamhuongdtvbhp@gmail.com>
2019-06-04 12:26:17 +0700

    form them gv

--- app/views/teacher/teacher-form.php
+++ app/views/teacher/teacher-form.php
@@ -26,28 +26,30 @@ require_once '../app/views/home/menu.php';
                 <?php if (!empty($data['data'])) {
                     while ($row = $data['data']->fetch_assoc()) {
                         ?>
-                        <form action="?url=teacher/update&id=<?= $row['id'] ?>" method="post">
-                            <input type="hidden" name="id" value="<?= $row['id'] ?>">
-                            <input class="form-control" required name="name_teacher" type="text" placeholder="Tên giáo viên"
-                                   value="<?= $row['name_teacher'] ? $row['name_teacher'] : '' ?>"><br>
-                            <input class="form-control" required name="address" type="text" placeholder="Địa chỉ"
-                                   value="<?= $row['address'] ? $row['address'] : '' ?>"><br>
-                            <div class="input-group date form_date" data-date="" data-date-format="yyyy/mm/dd"
-                                 data-link-field="dtp_input2" data-link-format="yyyy/mm/dd">
-                                <input name="date_of_birth" class="form-control" size="16" type="text"
-                                       value="<?= $row['date_of_birth'] ? $row['date_of_birth'] : '' ?>" readonly>
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
-                            </div>
-                            <br>
-                            <input class="form-control" required name="ability" type="text" placeholder="Lĩnh vực chuyên môn"
-                                   value="<?= $row['ability'] ? $row['ability'] : '' ?>"><br>
-                            <input class="form-control" required name="class_teacher" type="text" placeholder="Chủ nhiệm lớp"
-                                   value="<?= $row['class_teacher'] ? $row['class_teacher'] : '' ?>"><br>
-                            <input class="form-control" type="text" placeholder="Giới tính"
-                                   value="<?= $row['sex'] ? $row['sex'] : '' ?>"><br>
-                            <input class="btn btn-success" type="submit" name="submit_teacher" value="Cập nhật giáo viên">
-                        </form>
+                                <div class="col-md-12">
+                                    <form action="?url=teacher/update&id=<?= $row['id'] ?>" method="post">
+                                        <input type="hidden" name="id" value="<?= $row['id'] ?>">
+                                        <input class="form-control" required name="name_teacher" type="text" placeholder="Tên giáo viên"
+                                               value="<?= $row['name_teacher'] ? $row['name_teacher'] : '' ?>"><br>
+                                        <input class="form-control" required name="address" type="text" placeholder="Địa chỉ"
+                                               value="<?= $row['address'] ? $row['address'] : '' ?>"><br>
+                                        <div class="input-group date form_date" data-date="" data-date-format="yyyy/mm/dd"
+                                             data-link-field="dtp_input2" data-link-format="yyyy/mm/dd">
+                                            <input name="date_of_birth" class="form-control" size="16" type="text"
+                                                   value="<?= $row['date_of_birth'] ? $row['date_of_birth'] : '' ?>" readonly>
+                                            <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
+                                            <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
+                                        </div>
+                                        <br>
+                                        <input class="form-control" required name="ability" type="text" placeholder="Lĩnh vực chuyên môn"
+                                               value="<?= $row['ability'] ? $row['ability'] : '' ?>"><br>
+                                        <input class="form-control" required name="class_teacher" type="text" placeholder="Chủ nhiệm lớp"
+                                               value="<?= $row['class_teacher'] ? $row['class_teacher'] : '' ?>"><br>
+                                        <input class="form-control" type="text" placeholder="Giới tính"
+                                               value="<?= $row['sex'] ? $row['sex'] : '' ?>"><br>
+                                        <input class="btn btn-success" type="submit" name="submit_teacher" value="Cập nhật giáo viên">
+                                    </form>
+                                </div>
                     <?php }
                 } else { ?>
                     <form action="?url=teacher/addTeacher" method="post">
