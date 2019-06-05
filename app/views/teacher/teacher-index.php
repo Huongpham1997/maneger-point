8ca1796f0c95daddb53e9759220e92e25c318048
Hường <phamhuongdtvbhp@gmail.com>
2019-06-05 20:28:07 +0700

    ok lan 2

--- app/views/teacher/teacher-index.php
+++ app/views/teacher/teacher-index.php
@@ -59,7 +59,7 @@
                                             <?php
                                             if (!empty($row['image'])) {
                                                 ?>
-                                                <img width="80" height="80" src="/statics/images/images_teacher/<?= $row['image'] ?>">
+                                                <img width="100" height="100" src="/statics/images/images_teacher/<?= $row['image'] ?>">
                                                 <?php
                                             } else {
                                                 ?>
